132ed719d5357f48427bd68a5374ad29a030b2f6
Alexandre DOYEN <alex.doyen20042000@gmail.com>
2025-03-30 10:08:09 +0200

    kwrite wrapper implementation

--- Plugins/IOKernelRW/iokernelrw_krw_plugin.c
+++ Plugins/IOKernelRW/iokernelrw_krw_plugin.c
@@ -1,4 +1,6 @@
 #include <IOKit/IOKitLib.h>
+#include <cstddef>
+#include <cstdint>
 #include <iokernelrw.h>
 #include <mach/arm/kern_return.h>
 #include <mach/kern_return.h>
@@ -32,12 +34,36 @@ int kread_wrapper(uint64_t from, void* to, size_t len)
     return result;
 }
 
+int kwrite_wrapper(void* from, uint64_t to, size_t len)
+{
+    kern_return_t result_write;
+    int result;
+
+    if (krw_client == IO_OBJECT_NULL)
+    {
+        krw_client = iokernelrw_open();
+    }
+
+    result_write = iokernelrw_write(krw_client, from, to, len);
+
+    if (result_write == KERN_SUCCESS)
+    {
+        result = 0;
+    }
+    else
+    {
+        result = EDEVERR;
+    }
+
+    return result;
+}
+
 int krw_plugin_initializer(krw_handlers_t handlers)
 {
     //handlers->version = TODO;
     //handlers->kbase = TODO;
     handlers->kread = kread_wrapper;
-    //handlers->kwrite = TODO;
+    handlers->kwrite = kwrite_wrapper;
     //handlers->kmalloc = TODO;
     //handlers->kdealloc = TODO;
     //handlers->kcall = TODO;
